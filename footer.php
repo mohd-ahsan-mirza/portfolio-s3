5be9c9436c0d571d97f7142747c64eb3d89715cd
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-25 02:54:02 -0400

    Commit 23

---
+++ footer.php
@@ -0,0 +1,51 @@
+	<a class='openDialogLink' href="#openModal">Open Modal</a>
+	<div id="openModal" class="modalDialog">
+		<div>
+			<a href="#close" title="Close" class="close">X</a>
+			<p></p>
+		</div>
+	</div>
+	<footer id="footer">
+			<div class="inner">
+
+					<h2>Contact Me</h2>
+
+					<form action="#" method="post" class='contact-form'>
+
+						<div class="field half first">
+							<label for="name">Name</label>
+							<input class='input-name' name="name" id="name" type="text" placeholder="Name">
+							<div class='error name'></div>
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input class='input-email' name="email" id="email" type="email" placeholder="Email">
+							<div class='error email'></div>
+						</div>
+						<div class="field">
+							<label for="subject">Subject</label>
+							<input class='input-subject' name="subject" id="subject" type="text" placeholder="Subject">
+							<div class='error subject'></div>
+						</div>
+						<div class="field">
+							<label for="message">Message</label>
+							<textarea class='input-message' name="message" id="message" rows="6" placeholder="Message"></textarea>
+							<div class='error message'></div>
+						</div>
+						<ul class="actions">
+							<li><input class='input-submit' value="Send Message" class="button alt" type="submit"></li>
+						</ul>
+					</form>
+
+					<ul class="icons">
+						<li><a href='https://ca.linkedin.com/in/muhammad-ahsan-mirza-460b6378' class="icon round fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href='https://github.com/mohd-ahsan-mirza' class="icon round fa-github"><span class="label">GitHub</span></a></li>
+						<li><a style="color: inherit;" href='https://ahsanmirza.com/blogs' class="icon round fa-wordpress"><span class="label">GitHub</span></a></li></ul>
+					<div class="copyright">
+						&copy; Copyright 2019 Ahsan Mirza
+					</div>
+
+				</div>
+	</footer>
+	</body>
+</html>
